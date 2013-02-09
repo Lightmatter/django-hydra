ec54b777a2ed57117c513fc747a9a5624b64f245
Ben Beecher <benbeecher@gmail.com>
2013-02-09 14:17:09 -0500

    removing default conf

--- project/project/settings/local.py
+++ project/project/settings/local.py
@@ -3,10 +3,10 @@ from .base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'mapsite',                      # Or path to database file if using sqlite3.
+        'NAME': '',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'mapsite',
-        'PASSWORD': 'mapsite',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
