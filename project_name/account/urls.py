f2d7c991e851894fdc2ee28ca3870e727e4ae5d2
Ben Beecher <benbeecher@gmail.com>
2013-11-18 01:37:13 -0500

    moving more of the logic into the template

--- project_name/account/urls.py
+++ project_name/account/urls.py
@@ -1,7 +1,21 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic.base import TemplateView
+from .views import RegistrationView
 
+urlpatterns = patterns('',
+                       url(r'^register/$',
+                           RegistrationView.as_view(),
+                           name='registration_register'),
+)
+
+
+
+###Simple backend doesn't do email confirmation
+# urlpatterns += patterns('app.views',
+#     (r'', include('registration.backends.simple.urls')),
+# )
+
+###default backend does  email confirmation
 
 urlpatterns = patterns('app.views',
-    (r'', include('registration.backends.default.urls')),   #
+    (r'', include('registration.backends.default.urls')),
 )
--- project_name/account/views.py
+++ project_name/account/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+from registration.backends.simple.views import RegistrationView as SimpleRegistrationView
+from registration.backends.default.views import RegistrationView as DefaultRegistrationView
+
+
+from .forms import RegistrationForm
+
+
+## Make sure the view's base class matches the backend we're importing from
+#class RegistrationView(SimpleRegistrationView):
+class RegistrationView(DefaultRegistrationView):
+    """ The class view that handles user registration. See
+    https://bitbucket.org/ubernostrum/django-registration/src/8f242e35ef7c004e035e54b4bb093c32bf77c29f/registration/backends/simple/views.py?at=default#cl-11
+    for an example of a simple way to use it
+    """
+    form_class = RegistrationForm
+
+    ##Stick registration logic here
+    def register(self, request, **cleaned_data):
+        new_user = super(RegistrationView, self).register(request, **cleaned_data)
+        return new_user
