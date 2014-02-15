af7c8c51acb11871499f331f75b711b7faf9e541
Ben Beecher <benbeecher@gmail.com>
2014-02-15 13:14:10 -0500

    adding in settings file for project specific settings

---
+++ project_name/project_name/settings/app/project_name.py
@@ -0,0 +1,4 @@
+STRIPE_SECRET = "YOUR-SECRET-API-KEY"
+STRIPE_PUBLISHABLE = "YOUR-PUBLISHABLE-API-KEY"
+#what model stores the stripe user - 9/10 times this will be our user
+ZEBRA_CUSTOMER_MODEL = "account.User"
