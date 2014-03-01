77ea0a4fda1ae311a6d63fb1eab8362b77f0d5b3
Ben Beecher <benbeecher@gmail.com>
2014-03-01 17:58:49 -0500

    Adding gzip mixin

--- project_name/app/storage.py
+++ project_name/app/storage.py
@@ -1,7 +1,10 @@
-from staticfiles.storage import CachedStaticFilesStorage
-
 from pipeline.storage import GZIPMixin
+from staticfiles.storage import CachedFilesMixin
+
+from pipeline.storage import PipelineMixin
+
+from storages.backends.s3boto import S3BotoStorage
 
 
-class GZIPCachedStorage(GZIPMixin, CachedStaticFilesStorage):
+class S3PipelineStorage(PipelineMixin, CachedFilesMixin, GZIPMixin, S3BotoStorage):
     pass
