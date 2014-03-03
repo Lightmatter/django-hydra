d929ddb1ccaf0a226728ce8e48e0043554c1a352
Ben Beecher <benbeecher@gmail.com>
2014-03-03 13:36:41 -0500

    bugfix for file url

--- util/util.py
+++ util/util.py
@@ -9,7 +9,7 @@ def file_url(category):
     def inner(instance, filename):
         r = re.compile(r'[^\S]')
         filename = r.sub('', filename)
-        now = datetime.now()
+        now = datetime.datetime.now()
         timestamp = int(time.time())
         return 'uploads/{0}/{1.year:04}/{1.month:02}/{1.day:02}/{2}/{3}'.format( \
                 category, now, timestamp, filename)
