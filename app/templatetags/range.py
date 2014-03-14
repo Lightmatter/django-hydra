ce69a345b95fadd9de85e5e53782b97ef777e57b
Ben Beecher <benbeecher@gmail.com>
2014-03-14 16:24:57 -0400

    trying verbatium again

--- app/templatetags/range.py
+++ app/templatetags/range.py
@@ -7,5 +7,20 @@ register = Library()
 def range(value):
     """
     Filter - returns a list containing range made from given value
+    Usage (in template): (verbatim tags to account for start project)
+    {% verbatim %}
+    <ul>{% for i in 3|range %}
+      <li>{{ i }}. Do something</li>
+    {% endfor %}</ul>
+    {% endverbatim %}
+
+
+    Results with the HTML:
+    <ul>
+      <li>0. Do something</li>
+      <li>1. Do something</li>
+      <li>2. Do something</li>
+    </ul>
+    Instead of 3 one may use the variable set in the views
     """
     return range(value)
