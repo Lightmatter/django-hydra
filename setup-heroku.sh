3fe5648ee0aca799aaa6e4fd67fc90e32321bd6d
Ben Beecher <benbeecher@gmail.com>
2014-04-06 23:51:50 -0400

    adding in heroku script

---
+++ setup-heroku.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+ENV_NAME="{{ project_name }}"
+ENV_OPSTS="--no-site-packages --distribute"
+
+
+unset PYTHONDONTWRITEBYTECODE
+os="`uname -a`"
+if [[ "$os" == *Linux* ]]; then
+    source /etc/bash_completion.d/virtualenvwrapper
+else
+    source `which virtualenvwrapper.sh`
+fi
+
+
+workon $ENV_NAME
+heroku create $ENV_NAME-staging --remote staging
+heroku create $ENV_NAME-prod --remote prod
+heroku addons:add pgbackups --app $ENV_NAME-prod
+heroku addons:add pgbackups --app $ENV_NAME-staging
+heroku addons:add mandrill:starter --app $ENV_NAME-staging
+heroku addons:add mandrill:starter --app $ENV_NAME-prod
+heroku addons:add newrelic --app $ENV_NAME-staging
+heroku addons:add newrelic --app $ENV_NAME-prod
+heroku config:set SECRET_KEY=`python -c 'import random; print "".join([random.choice("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)") for i in range(50)])'` --app $ENV_NAME-staging
+heroku config:set SECRET_KEY=`python -c 'import random; print "".join([random.choice("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)") for i in range(50)])'` --app $ENV_NAME-prod
+heroku config:set DJANGO_SETTINGS_MODULE=$ENV_NAME.settings.heroku-staging --app $ENV_NAME-staging
+heroku config:set DJANGO_SETTINGS_MODULE=$ENV_NAME.settings.heroku --app $ENV_NAME-prod
+
+heroku config:set AWS_SECRET_ACCESS_KEY="" --app $ENV_NAME-prod
+heroku config:set AWS_ACCESS_KEY_ID="" --app $ENV_NAME-prod
+heroku config:set AWS_STORAGE_BUCKET_NAME=$ENV_NAME-prod --app $ENV_NAME-prod
+heroku config:set AWS_SECRET_ACCESS_KEY="" --app $ENV_NAME-staging
+heroku config:set AWS_ACCESS_KEY_ID="" --app $ENV_NAME-staging
+heroku config:set AWS_STORAGE_BUCKET_NAME=$ENV_NAME-staging --app $ENV_NAME-staging
+
+heroku config:set STRIPE_PUBLIC_KEY="" --app $ENV_NAME-staging
+heroku config:set STRIPE_PUBLIC_KEY="" --app $ENV_NAME-prod
+heroku config:set STRIPE_SECRET_KEY="" --app $ENV_NAME-staging
+heroku config:set STRIPE_SECRET_KEY="" --app $ENV_NAME-prod
+
+
+
+git push staging master
+git push prod master
+heroku run python manage.py syncdb --migrate --app $ENV_NAME-staging
+heroku run python manage.py syncdb --migrate --app $ENV_NAME-prod
