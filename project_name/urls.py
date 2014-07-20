d5d2552e62c2a466750150cacd883a06603818e2
Ben Beecher <benbeecher@gmail.com>
2014-07-20 15:25:54 -0400

    better url layout and adding social registration urls by default

--- project_name/urls.py
+++ project_name/urls.py
@@ -8,19 +8,19 @@ from filebrowser.sites import site
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'', include('app.urls')),
+    url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'account/', include('account.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-
     url(r'^admin/filebrowser/', include(site.urls)),
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include('smuggler.urls')), # put it before admin url
     url(r'^admin/', include(admin.site.urls)),
     url(r"^payments/", include("payments.urls")),
+    url(r'', include('app.urls')),
 )
 
 
