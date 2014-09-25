7723b6c51b957a7cc936c515edfe7717de122854
Josh Schneier <josh.schneier@gmail.com>
2014-09-25 12:50:25 -0400

    fix MEDIA_ROOT in local.py

--- project_name/settings/local.py
+++ project_name/settings/local.py
@@ -23,7 +23,7 @@ CACHES = {
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = PROJECT_ROOT.child("media")
+MEDIA_ROOT = PROJECT_ROOT
 MEDIA_URL = "/media/"
 
 # Absolute path to the directory static files should be collected to.
