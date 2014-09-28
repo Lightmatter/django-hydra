1b45218f1438afba898dd935f2ff78f883c00e84
Ben Beecher <benbeecher@gmail.com>
2014-09-28 19:21:36 -0400

    making sure to test migrations

--- project_name/settings/wercker.py
+++ project_name/settings/wercker.py
@@ -11,7 +11,7 @@ DATABASES = {
     )
 }
 
-
+SOUTH_TESTS_MIGRATE = True
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
