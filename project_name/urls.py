694b98f70352d1ad65d07a074f87be0d84de40b4
Josh Schneier <josh.schneier@gmail.com>
2014-12-09 12:12:31 -0500

    autodiscover happens automatically in django 1.7

--- project_name/urls.py
+++ project_name/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-admin.autodiscover()
 
 from filebrowser.sites import site
 from django.views.generic.base import RedirectView as rv
