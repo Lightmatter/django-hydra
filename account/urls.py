ad34def9f722f0d295f48c9e493d9afb66c881df
Josh Schneier <josh.schneier@gmail.com>
2015-09-18 19:16:34 -0400

    remove patterns, it is deprecated

--- account/urls.py
+++ account/urls.py
@@ -1,21 +1,12 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from .views import RegistrationView
 
-urlpatterns = patterns('',
-                       url(r'^register/$',
-                           RegistrationView.as_view(),
-                           name='registration_register'),
-)
 
+urlpatterns = [
+    url(r'^register/$', RegistrationView.as_view(), name='registration_register'),
+]
 
-
-###Simple backend doesn't do email confirmation
-urlpatterns += patterns('app.views',
+# Simple backend doesn't do email confirmation
+urlpatterns += [
     (r'', include('registration.backends.simple.urls')),
-)
-
-###default backend does  email confirmation
-
-# urlpatterns = patterns('app.views',
-#     (r'', include('registration.backends.default.urls')),
-# )
+]
--- project_name/urls.py
+++ project_name/urls.py
@@ -1,34 +1,26 @@
-from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
+from django.conf import settings
+from django.conf.urls import include, url
 from django.contrib import admin
 
 from django.views.generic.base import RedirectView as rv
 
-urlpatterns = patterns('',
-    # Examples:
+urlpatterns = [
     url(r'^favicon\.ico$', rv.as_view(url='/static/img/favicon.ico', permanent=True)),
     url(r'account/', include('account.urls')),
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^djangojs/', include('djangojs.urls')),
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'', include('app.urls')),
-)
+]
 
 
-from django.conf import settings
-#TODO dev
 if settings.DEBUG:
-    from djangojs.views import JasmineView, QUnitView
+    from djangojs.views import QUnitView
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
     urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += patterns('',
+    urlpatterns += [
         url(r'^qunit/$', QUnitView.as_view(js_files='js/tests/*.tests.js'), name='qunit_view'),
         url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
             'document_root': settings.MEDIA_ROOT,
         }),
-   )
+   ]
