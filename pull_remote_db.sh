34b1aea79049ab720bd0661ba2d598d77cd191d5
Ben Beecher <benbeecher@gmail.com>
2015-09-25 16:07:00 -0400

    adding pull remote db script

---
+++ pull_remote_db.sh
@@ -0,0 +1,6 @@
+ENV_NAME="{{ project_name }}"
+
+dropdb $ENV_NAME
+heroku pg:pull DATABASE_URL $ENV_NAME --app $ENV_NAME-dev
+for tbl in `psql -qAt -c "select tablename from pg_tables where schemaname = 'public';" $ENV_NAME` ; do  psql -c "alter table \"$tbl\" owner to $ENV_NAME" $ENV_NAME ; done
+for tbl in `psql -qAt -c "select sequence_name from information_schema.sequences where sequence_schema = 'public';" $ENV_NAME` ; do  psql -c "alter table \"$tbl\" owner to $ENV_NAME" $ENV_NAME ; done
