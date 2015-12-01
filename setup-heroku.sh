84b73ba25e24e73a68feec8764de305985a59071
Josh Schneier <josh.schneier@gmail.com>
2015-11-30 21:33:39 -0500

    randomize the python hashseed on startup to prevent ddos attacks

--- setup-heroku.sh
+++ setup-heroku.sh
@@ -16,7 +16,7 @@ heroku addons:create newrelic --app $ENV_NAME-prod
 
 heroku addons:create redistogo --app $ENV_NAME-prod
 
-
+heroku config:set PYTHONHASHSEED=random --app $ENV_NAME-prod
 heroku config:set AWS_SECRET_ACCESS_KEY="" --app $ENV_NAME-prod
 heroku config:set AWS_ACCESS_KEY_ID="" --app $ENV_NAME-prod
 heroku config:set AWS_STORAGE_BUCKET_NAME=$ENV_NAME-prod --app $ENV_NAME-prod
@@ -34,12 +34,14 @@ heroku fork --to $ENV_NAME-dev --from $ENV_NAME-prod
 heroku config:set SECRET_KEY=`python -c 'import random; print("".join([random.choice("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)") for i in range(50)]))'` --app $ENV_NAME-dev
 heroku config:set SECRET_KEY=`python -c 'import random; print("".join([random.choice("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)") for i in range(50)]))'` --app $ENV_NAME-staging
 
+heroku config:set PYTHONHASHSEED=random --app $ENV_NAME-dev
 heroku config:set AWS_SECRET_ACCESS_KEY="" --app $ENV_NAME-dev
 heroku config:set AWS_ACCESS_KEY_ID="" --app $ENV_NAME-dev
 heroku config:set AWS_STORAGE_BUCKET_NAME=$ENV_NAME-dev --app $ENV_NAME-dev
 heroku config:set STRIPE_PUBLIC_KEY="" --app $ENV_NAME-dev
 heroku config:set STRIPE_SECRET_KEY="" --app $ENV_NAME-dev
 
+heroku config:set PYTHONHASHSEED=random --app $ENV_NAME-staging
 heroku config:set AWS_SECRET_ACCESS_KEY="" --app $ENV_NAME-staging
 heroku config:set AWS_ACCESS_KEY_ID="" --app $ENV_NAME-staging
 heroku config:set AWS_STORAGE_BUCKET_NAME=$ENV_NAME-staging --app $ENV_NAME-staging
