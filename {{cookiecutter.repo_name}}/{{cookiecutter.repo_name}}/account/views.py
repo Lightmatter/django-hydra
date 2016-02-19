10d89f3ad792012a346b57a47dfe7efce625bfd1
Matt Long <matthewhl@gmail.com>
2016-02-19 14:34:30 -0500

    first pass

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/account/views.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/account/views.py
@@ -56,31 +56,47 @@
 {% else %}
 
 from django.shortcuts import render
+from djoser.views import (
+    RegistrationView as DjoserUserRegistrationView,
+    SetPasswordView as DjoserSetPasswordView,
+    LoginView as DjoserLoginView,
+    LogoutView as DjoserLogoutView,
+    PasswordResetView as DjoserPasswordResetView,
+    PasswordResetConfirmView as DjoserPasswordResetConfirmView,
+    ActivationView as DjoserActivationView
+    SetUsernameView as DjoserSetUsernameView
+    UserView as DjoserUserView
+)
 
-from registration.backends.simple.views import RegistrationView as SimpleRegistrationView
-from registration.backends.default.views import RegistrationView as DefaultRegistrationView
 
 
-from .forms import RegistrationForm
+# Primary User Views
 
+class LoginView(DjoserLoginView):
+    pass
 
-# Make sure the view's base class matches the backend we're importing from
-#class RegistrationView(DefaultRegistrationView):
-class RegistrationView(SimpleRegistrationView):
-    """ The class view that handles user registration. See
-    https://bitbucket.org/ubernostrum/django-registration/src/8f242e35ef7c004e035e54b4bb093c32bf77c29f/registration/backends/simple/views.py?at=default#cl-11
-    for an example of a simple way to use it
-    """
-    form_class = RegistrationForm
+class LogoutView(DjoserLogoutView):
+    pass
 
-    # Stick extra registration logic here
-    def register(self, form, **cleaned_data):
-        new_user = super().register(form, **cleaned_data)
-        return new_user
+class RegistrationView(DjoserRegistrationView):
+    pass
 
-    def get_success_url(self, user):
-        """
-        Return the url a user should be redirected to after registration
-        """
-        return self.request.GET.get('next', '/')
-{% endif %}
+class UserView(DjoserUserView):
+    pass
+
+# Utility User Views
+
+class SetUserPassword(DjoserSetPasswordView):
+    pass
+
+class SetUsernameView(DjoserSetUsernameView):
+    pass
+
+class PasswordResetView(DjoserPasswordResetView):
+    pass
+
+class PasswordResetConfirmView(DjoserPasswordResetConfirmView):
+    pass
+
+class ActivationView(DjoserActivationView):
+    pass
