856bddcca5294a923b6bc908e2bbbe3eed2707d8
Matt Long <matthewhl@gmail.com>
2016-03-16 11:42:49 -0400

    alter user superuser for postgres; some more settings fiddling

--- {{cookiecutter.repo_name}}/scripts/start.sh
+++ {{cookiecutter.repo_name}}/scripts/start.sh
@@ -44,6 +44,7 @@ RESULT=`psql -l | grep "{{ cookiecutter.repo_name }}" | wc -l | awk '{print $1}'
 if test $RESULT -eq 0; then
     echo "Creating Database";
     psql -c "create role {{ cookiecutter.repo_name }} with createdb encrypted password '{{ cookiecutter.repo_name }}' login;"
+    psql -c "alter user {{ cookiecutter.repo_name }} superuser;"
     psql -c "create database {{ cookiecutter.repo_name }} with owner {{ cookiecutter.repo_name }};"
 else
     echo "Database exists"
