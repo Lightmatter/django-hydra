54826891927426ee0f0cd2bcf9e27983c4b8a638
Matt Long <matthewhl@gmail.com>
2016-03-16 14:00:07 -0400

    some more templating

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/account/tests.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/account/tests.py
@@ -2,6 +2,10 @@ from django.test import TestCase
 from django.core.urlresolvers import reverse
 from django.contrib.auth.hashers import make_password
 
+{%- if cookiecutter.django_registration == 'y' -%}
+from .forms import RegistrationForm
+{%- endif -%}
+
 from .models import User
 from .recipies import user_recipe
 
@@ -10,6 +14,67 @@ class UserManager(TestCase):
     def test_create_user(self):
         user = User.objects.create_user("jonnyrico@fednet.gov", password="iwanttoknowmore")
         User.objects.get(id=user.id)
+
+{%- if cookiecutter.django_registration == 'y' -%}
+
+class RegistrationTest(TestCase):
+    def setUp(self):
+        self.user = user_recipe.prepare()
+        self.form_keys = RegistrationForm.base_fields.keys()
+        self.form_data = {k: v for (k, v) in self.user.__dict__.items() if k in self.form_keys}
+        self.form_data['tos'] = "True"
+        self.form_data['password1'] = self.form_data['password2'] = "bugssuck"
+
+    def test_user_registration_fails(self):
+        """
+        on failed registration, keep the user on the same page and ask for a fix
+        """
+        del self.form_data['tos']
+        url = reverse("registration_register")
+        response = self.client.post(url, self.form_data)
+        actual = response.status_code
+        expected = 200
+        self.assertEqual(actual, expected)
+
+    def test_user_registration_fails_passwords_must_match(self):
+        self.form_data['password2'] = "bugs rule"
+
+        url = reverse("registration_register")
+        response = self.client.post(url, self.form_data)
+        actual = response.status_code
+        expected = 200
+        self.assertEqual(actual, expected)
+        actual = response.context['form'].errors['__all__']
+        expected = ["The two password fields didn't match."]
+        self.assertEqual(actual, expected)
+
+    def test_user_registration_fails_unique_email(self):
+        url = reverse("registration_register")
+        actual = self.client.post(url, self.form_data)
+        expected = reverse("home")
+        self.assertRedirects(actual, expected)
+        User.objects.get(email=self.user.email)
+        response = self.client.post(url, self.form_data)
+        actual = response.status_code
+        expected = 200
+        self.assertEqual(actual, expected)
+        actual = response.context['form'].errors['email']
+        expected = ["This email address is already in use. Please supply a different email address."]
+        self.assertEqual(actual, expected)
+
+
+    def test_user_registration_works(self):
+        """
+        on successful registration, move user to either
+        their profile page or the next page specified in the querystring
+        """
+        url = reverse("registration_register")
+        actual = self.client.post(url, self.form_data)
+        expected = reverse("home")
+        self.assertRedirects(actual, expected)
+        User.objects.get(email=self.user.email)
+
+{%- endif -%}
 
 class LoginTest(TestCase):
     def setUp(self):
--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/account/urls.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/account/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, include
+{% if cookiecutter.django_registration == 'n' %}
 from .views import LoginView, LogoutView, RegistrationView, PasswordResetView, PasswordResetConfirmView
-
 
 authurls = [
     url(r'^login/', LoginView.as_view(), name='user-login'),
     url(r'^logout/', LogoutView.as_view(), name='user-logout'),
+{%- if cookiecutter.djoser != 'basic' -%}
     url(r'^register/user/', RegistrationView.as_view(), name='user-register'),
     url(r'^password/reset/$', PasswordResetView.as_view(), name='password-reset'),
     url(r'^password/reset/confirm/$', PasswordResetConfirmView.as_view(), name='password-confirm'),
