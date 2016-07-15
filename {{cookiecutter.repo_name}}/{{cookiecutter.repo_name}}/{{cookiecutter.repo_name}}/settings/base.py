4bdf33e9cff3db7374c92aab296b172ee45c88f1
Jacob Kent <kentjakel@wustl.edu>
2016-07-15 18:13:36 -0400

    clean up base settings, remove lingering broswer checks

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/base.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/base.py
@@ -240,7 +240,6 @@ PIPELINE = {
         'vendor': {
             'source_filenames': (
                 'js/vendor/jquery-1.11.0.min.js',
-                'js/djangojs/django.js',
                 'js/vendor/select2.min.js',
             ),
             'output_filename': 'js/vendor.js',
