9473c257f299527af20c8136e687fc53f799ed55
Josh Schneier <josh.schneier@gmail.com>
2016-08-30 12:50:07 -0400

    lowercase the context processor

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/home/context_processors.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/home/context_processors.py
@@ -2,7 +2,7 @@ from django.conf import settings as django_settings
 
 def settings(request):
     settings = {
-        'SETTINGS': django_settings,
+        'settings': django_settings,
     }
 
     return settings
