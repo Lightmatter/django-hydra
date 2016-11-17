745af99ca79d24d49d9ea2e0281659897734a37f
Sam Epstein <sam@samepstein.com>
2016-11-17 18:36:09 -0500

    Cleanup the post_gen_project script
    Fixes the case where $PROJECT_HOME doesn't exist

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,33 +1,29 @@
 #!/bin/bash
+set -e +o pipefail
 
 source `which virtualenvwrapper.sh`
 unset PYTHONDONTWRITEBYTECODE
 
 ENV_NAME="{{ cookiecutter.repo_name }}"
-ENV_PATH="${PROJECT_HOME}/${ENV_NAME}"
+ENV_PATH="${PROJECT_HOME:?}/$ENV_NAME"
 
 workon $ENV_NAME
 cd $ENV_PATH
 
-if [ ! -d ${HOME}/.pip-packages ]
-then
-    mkdir -p ${HOME}/.pip-packages
+if [ ! -d "${HOME:?}/.pip-packages" ]; then
+    mkdir -p "$HOME/.pip-packages"
 fi
 
-
-
-if [  -d $WORKON_HOME/{{ cookiecutter.repo_name }}/build/ ]
-
-then
-    rm -rf $WORKON_HOME/{{ cookiecutter.repo_name }}/build/
+if [ -d "${WORKON_HOME:?}/{{ cookiecutter.repo_name }}/build/" ]; then
+    rm -rf "$WORKON_HOME/{{ cookiecutter.repo_name }}/build/"
 fi
 
+pip download -r requirements-dev.txt -d "$HOME/.pip-packages" --exists-action w
+pip install --no-index --exists-action w --find-links="file://$HOME/.pip-packages/" -r requirements-dev.txt
 
-pip download -r requirements-dev.txt -d ${HOME}/.pip-packages --exists-action w
-pip install --no-index --exists-action w --find-links=file://${HOME}/.pip-packages/ -r requirements-dev.txt
-
+# Create the DB if necessary
 RESULT=`psql -l | grep "{{ cookiecutter.repo_name }}" | wc -l | awk '{print $1}'`;
-if test $RESULT -eq 0; then
+if test "$RESULT" -eq 0; then
     echo "Creating Database";
     psql -c "create role {{ cookiecutter.repo_name }} with createdb encrypted password '{{ cookiecutter.repo_name }}' login;"
     psql -c "alter user {{ cookiecutter.repo_name }} superuser;"
@@ -36,7 +32,7 @@ else
     echo "Database exists"
 fi
 
-export DJANGO_SETTINGS_MODULE=$ENV_NAME.$ENV_NAME.settings.local
+export DJANGO_SETTINGS_MODULE="$ENV_NAME.$ENV_NAME.settings.local"
 
 python manage.py migrate
 
