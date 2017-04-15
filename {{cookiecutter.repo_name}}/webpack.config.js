ef5e0228858cecc44340e37b8a67b496ccf5a886
Ben Beecher <benbeecher@gmail.com>
2017-04-14 19:54:43 -0700

    moving files around, use import vs require

--- {{cookiecutter.repo_name}}/webpack.config.js
+++ {{cookiecutter.repo_name}}/webpack.config.js
@@ -22,66 +22,66 @@ PATHS.fonts = path.join(PATHS.static_source, "fonts");
 
 
 module.exports = {
-    PATHS: PATHS,
-    context: __dirname,
-    entry: [
-        path.join(PATHS.js, "main"),
-        path.join(PATHS.sass, "style.scss")
-    ],
-    output: {
-        path: PATHS.build,
-        filename: "[name]-[hash].js",
-    },
+  PATHS: PATHS,
+  context: __dirname,
+  entry: [
+    path.join(PATHS.js, "main"),
+    path.join(PATHS.sass, "style.scss")
+  ],
+  output: {
+    path: PATHS.build,
+    filename: "[name]-[hash].js",
+  },
 
-    plugins: [
-        new webpack.DefinePlugin({
-          'process.env': {
-            'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-          }
-        }),
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        new ExtractTextPlugin("[name]-[hash].css", {allChunks:true}),
-        new BundleTracker({filename:'./{{ cookiecutter.repo_name }}/webpack-stats.json'}),
-    ],
-    module: {
-        loaders: [
-            {
-                //keep this one first, as in dev-server we depend on the ordering
-                test: /\.scss$/,
-                loader: ExtractTextPlugin.extract("style", ["css?sourceMap", "postcss", "sass?sourceMap"], {publicPath:'.'}),
-                include: PATHS.sass
-            },{
-                test: /\.(ttf|eot|otf|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "url-loader",
-                include: PATHS.fonts
-            },{
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract("style", ["css?sourceMap", "postcss"]),
-                include: PATHS.css
-            },{
-                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "url-loader?limit=464600&minetype=application/font-woff",
-                include: PATHS.fonts
-            },{
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: "babel-loader",
-                query: {
-                  presets: ['es2015']
-                }
-            }]
-    },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new ExtractTextPlugin("[name]-[hash].css", {allChunks:true}),
+    new BundleTracker({filename:'./{{ cookiecutter.repo_name }}/webpack-stats.json'}),
+  ],
+  module: {
+    loaders: [
+      {
+        //keep this one first, as in dev-server we depend on the ordering
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract("style", ["css?sourceMap", "postcss", "sass?sourceMap"], {publicPath:'.'}),
+        include: PATHS.sass
+      },{
+        test: /\.(ttf|eot|otf|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader",
+        include: PATHS.fonts
+      },{
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract("style", ["css?sourceMap", "postcss"]),
+        include: PATHS.css
+      },{
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=464600&minetype=application/font-woff",
+        include: PATHS.fonts
+      },{
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ['es2015', 'react', 'airbnb']
+        }
+      }]
+  },
 
-    eslint: {
-        //failOnError: false,
-    },
+  eslint: {
+    //failOnError: false,
+  },
 
-    postcss: function () {
-        return [autoprefixer({browsers:"last 10 versions"})];
-    },
+  postcss: function () {
+    return [autoprefixer({browsers:"last 10 versions"})];
+  },
 
-    resolve: {
-        modulesDirectories: ['node_modules'],
-        extensions: ['', '.js', 'jsx']
-    },
+  resolve: {
+    modulesDirectories: ['node_modules'],
+    extensions: ['', '.js', 'jsx']
+  },
 };
