8254b05e98284eaf573b8cea4b80c933bf07096f
Ben Beecher <benbeecher@gmail.com>
2017-08-04 17:26:17 -0400

    updating dir locals to switch into virtualenvs

--- {{cookiecutter.repo_name}}/.dir-locals.el
+++ {{cookiecutter.repo_name}}/.dir-locals.el
@@ -1,9 +1,6 @@
-;; Pony mode config for the megacorp project
-((nil . ;; This applies these settings regardless of major mode
-
-     ((pony-settings (make-pony-project
-                         :python "~/.virtualenvs/{{cookiecutter.repo_name}}/bin/python"
-                         :pythonpath "~/.virtualenvs/{{cookiecutter.repo_name}}/lib/"
-                         :settings "{{cookiecutter.repo_name}}.{{cookiecutter.repo_name}}.settings.local"
-                         :appsdir "{{cookiecutter.repo_name}}")
-          ))))
+((nil
+  (pony-settings
+   (make-pony-project :python "~/.virtualenvs/{{cookiecutter.repo_name}}/bin/python" :pythonpath "~/.virtualenvs/{{cookiecutter.repo_name}}/lib/" :settings "{{cookiecutter.repo_name}}.{{cookiecutter.repo_name}}.settings.local" :appsdir "{{cookiecutter.repo_name}}")))
+ (pyenv-mode
+  (pyvenv-activate . t)
+  (pyvenv-workon . {{cookiecutter.repo_name}})))
