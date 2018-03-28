d26b8ff8a417be80e0c4fb535d9725701f46a505
Ben Beecher <benbeecher@gmail.com>
2018-03-28 15:28:17 -0400

    adding in choice enum

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/util/util.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/util/util.py
@@ -1,10 +1,12 @@
 import datetime
+import inspect
 import random
 import re
 import string
 import time
 
 from django.utils.deconstruct import deconstructible
+from enum import Enum
 
 
 @deconstructible
@@ -25,3 +27,16 @@ class file_url(object): # NOQA
 def random_string(length):
     alphanumeric = string.ascii_letters + string.digits
     return "".join(random.choice(alphanumeric) for i in range(length))
+
+
+class ChoiceEnum(Enum):
+
+    @classmethod
+    def choices(cls):
+        # get all members of the class
+        members = inspect.getmembers(cls, lambda m: not(inspect.isroutine(m)))
+        # filter down to just properties
+        props = [m for m in members if not(m[0][:2] == '__')]
+        # format into django choice tuple
+        choices = tuple([(str(p[1].value), p[0]) for p in props])
+        return choices
