ed8f67b5707b0015695fa9008714e4b80613fb40
Ben Beecher <ben@lightmatter.com>
2018-11-23 16:29:28 -0500

    Fix for favicon path

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/urls.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/urls.py
@@ -5,12 +5,14 @@ from django.views.generic.base import RedirectView
 from django.views.static import serve as static_serve
 
 urlpatterns = [
-    path(r'favicon\.ico',
-         RedirectView.as_view(url='/static/img/favicon.ico', permanent=True)),
-    path(r'account/', include('{{cookiecutter.repo_name}}.account.urls')),
-    path(r'admin/', admin.site.urls),
-    path('', include('social_django.urls', namespace='social')),
-    path(r'', include('{{cookiecutter.repo_name}}.home.urls')),
+    path(
+        r"favicon.ico",
+        RedirectView.as_view(url="/static/img/favicon.ico", permanent=True),
+    ),
+    path(r"account/", include("{{cookiecutter.repo_name}}.account.urls")),
+    path(r"admin/", admin.site.urls),
+    path("", include("social_django.urls", namespace="social")),
+    path(r"", include("{{cookiecutter.repo_name}}.home.urls")),
 ]
 
 if settings.DEBUG:
@@ -19,8 +21,8 @@ if settings.DEBUG:
 
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += [
-        path(r'media/<str:path>.*', static_serve, {
-            'document_root': settings.MEDIA_ROOT,
-        }),
-        path(r'__debug__/', include(debug_toolbar.urls)),
+        path(
+            r"media/<str:path>.*", static_serve, {"document_root": settings.MEDIA_ROOT}
+        ),
+        path(r"__debug__/", include(debug_toolbar.urls)),
     ]  # noqa
