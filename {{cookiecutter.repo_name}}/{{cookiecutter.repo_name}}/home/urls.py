fe1043bac1d5763c42df768acb9667f935ca9842
Brian Edelman <bd.edelman@gmail.com>
2019-11-15 16:23:25 -0500

    adds styleguide

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/home/urls.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/home/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.views.generic.base import TemplateView
 
-from .views import ExampleFormView, error
+from .views import ExampleFormView, Styleguide, error
 
 urlpatterns = [
     path(r"error/", error, name="error"),
+    path('styleguide/', Styleguide.as_view(), name='styleguide'),
     path(r"", TemplateView.as_view(template_name="index.jinja"), name="home"),
     path(r"form/", ExampleFormView.as_view(), name="Example Form"),
 ]
