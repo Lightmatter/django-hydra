ceb4e40aeecb9db1fb81de8c9ef52e20e7890ebc
Ben Beecher <ben@lightmatter.com>
2020-04-16 22:42:46 -0400

    setting up material-ui-formik

--- {{cookiecutter.repo_name}}/src/pages/signup.jsx
+++ {{cookiecutter.repo_name}}/src/pages/signup.jsx
@@ -1,136 +1,111 @@
-// import React from 'react';
-// import Header from '../components/header.jsx';
+import React from 'react';
 
-// import { Form, Formik } from 'formik';
-// import { registerUser } from 'models/user';
-// import InputField from '../components/forms/InputField.jsx';
-// import { useUser } from 'shared/hooks/user';
-// import { SignupSchema } from '/models/user';
+import { Form, Field, Formik } from 'formik';
+import { registerUser, SignupSchema, useUser } from 'models/user';
 
-// const SignUp = () => {
-//     const [user, fetchUser] = useUser();
-//     const [, addAlert] = useAlertContext();
+import Link from 'components/router/Link';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { TextField } from 'formik-material-ui';
 
-//     const DISCORD_ID = process.env.REACT_APP_DISCORD_ID;
-//     const DISCORD_REDIRECT_URL = encodeURIComponent(
-//         `${window.location.protocol}//${window.location.host}/discord-login`
-//     );
-//     const DISCORD_URL = `https://discordapp.com/api/oauth2/authorize?client_id=${DISCORD_ID}&redirect_uri=${DISCORD_REDIRECT_URL}&response_type=token&scope=identify%20email`;
+const SignUp = () => {
+    return (
+        <>
+            <Typography variant="h6">
+                <Link href="/login">Login</Link> or Sign Up
+            </Typography>
+            <Typography variant="h1">Register here, become a user!</Typography>
+            <Typography>Really excited to see you man!</Typography>
+            <Formik
+                initialValues={{
+                    first_name: '',
+                    last_name: '',
+                    email: '',
+                    password1: '',
+                    password2: '',
+                }}
+                validateOnChange
+                validationSchema={SignupSchema}
+                onSubmit={(values, actions) => {
+                    setTimeout(() => {
+                        registerUser(values)
+                            .then(response => {
+                                addAlert({ msg: 'Successfully signed up' });
+                                return response;
+                            })
+                            .then(response => {
+                                actions.setSubmitting(false);
+                                return response;
+                            })
+                            .catch(error => {
+                                actions.setSubmitting(false);
+                                if (error.non_field_errors) {
+                                    addAlert({
+                                        msg: error.non_field_errors,
+                                        severity: 'error',
+                                    });
+                                } else {
+                                    actions.setErrors(error);
+                                }
+                            });
+                    });
+                }}
+            >
+                <Form>
+                    <Field
+                        fullWidth
+                        component={TextField}
+                        name="first_name"
+                        label="First Name"
+                        placeholder="Enter First Name"
+                        helperText="Or your last name in Japanese"
+                    />
+                    <Field
+                        fullWidth
+                        name="last_name"
+                        component={TextField}
+                        label="Last Name"
+                        placeholder="Enter Last Name"
+                        helperText="Your ancestors will smile on you"
+                    />
+                    <Field
+                        fullWidth
+                        name="email"
+                        component={TextField}
+                        type="email"
+                        label="Email"
+                        placeholder="Enter Email Address"
+                        helperText="Will keep this as secret as your K/D ratio"
+                    />
+                    <Field
+                        fullWidth
+                        component={TextField}
+                        name="password1"
+                        type="password"
+                        label="Password"
+                        placeholder="Create Password"
+                        helperText="make the stars happy"
+                    />
+                    <Field
+                        fullWidth
+                        component={TextField}
+                        name="password2"
+                        type="password"
+                        label="Confirm Password"
+                        placeholder="Repeat Password"
+                        helperText="make the stars just as happy as before"
+                    />
+                    <Button variant="outlined" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </Formik>
+        </>
+    );
+};
+SignUp.getInitialProps = async ctx => {
+    debugger;
+    return {};
+};
 
-//     return (
-//         <>
-//             <Grid container className="signup" align="center">
-//                 <Grid item xs={12} className="login-signup-options">
-//                     <Typography variant="h6">
-//                         <Link to={AUTH_LOGIN}>Login</Link> or Sign Up
-//                     </Typography>
-//                 </Grid>
-//                 <Grid item xs={12} className="login-signup-options discord">
-//                     <Typography variant="h6">
-//                         <span className="discord-or">or</span>
-//                         <a href={DISCORD_URL} target="_blank" rel="noopener noreferrer">
-//                             Signup with Discord
-//                         </a>
-//                     </Typography>
-//                 </Grid>
-//                 <Grid item xs={8} className="login-signup-header">
-//                     <h1>Register here, become a user!</h1>
-//                     <p>
-//                         Anim et e culpa officia, ingeniis amet sint ut magna ut multos iudicem
-//                         tractavissent, se aliqua nescius. Arbitror iis arbitror, incurreret export.
-//                     </p>
-//                 </Grid>
-//                 <Grid item xs={12} className="auth-form">
-//                     <Formik
-//                         initialValues={{ first_name: '', last_name: '', email: '' }}
-//                         validateOnChange
-//                         validationSchema={SignupSchema}
-//                         onSubmit={(values, actions) => {
-//                             setTimeout(() => {
-//                                 registerUser(values)
-//                                     .then(response => {
-//                                         fetchUser();
-//                                         addAlert({ msg: 'Successfully signed up' });
-//                                         return response;
-//                                     })
-//                                     .then(response => {
-//                                         actions.setSubmitting(false);
-//                                         return response;
-//                                     })
-//                                     .catch(error => {
-//                                         actions.setSubmitting(false);
-//                                         if (error.non_field_errors) {
-//                                             addAlert({
-//                                                 msg: error.non_field_errors,
-//                                                 severity: 'error',
-//                                             });
-//                                         } else {
-//                                             actions.setErrors(error);
-//                                         }
-//                                     });
-//                             });
-//                         }}
-//                     >
-//                         <Form>
-//                             <InputField
-//                                 fullWidth
-//                                 name="first_name"
-//                                 type="text"
-//                                 label="First Name"
-//                                 placeholder="Enter First Name"
-//                                 helperText="Or your last name in Japanese"
-//                             />
-//                             <InputField
-//                                 fullWidth
-//                                 name="last_name"
-//                                 type="text"
-//                                 label="Last Name"
-//                                 placeholder="Enter Last Name"
-//                                 helperText="Your ancestors will smile on you"
-//                             />
-//                             <InputField
-//                                 fullWidth
-//                                 name="email"
-//                                 type="email"
-//                                 label="Email"
-//                                 placeholder="Enter Email Address"
-//                                 helperText="Will keep this as secret as your K/D ratio"
-//                             />
-//                             <InputField
-//                                 fullWidth
-//                                 name="username"
-//                                 type="text"
-//                                 label="Username"
-//                                 placeholder="Create Username"
-//                                 helperText="Your handle"
-//                             />
-
-//                             <InputField
-//                                 fullWidth
-//                                 name="password1"
-//                                 type="password"
-//                                 label="Password"
-//                                 placeholder="Create Password"
-//                                 helperText="make the stars happy"
-//                             />
-//                             <InputField
-//                                 fullWidth
-//                                 name="password2"
-//                                 type="password"
-//                                 label="Confirm Password"
-//                                 placeholder="Repeat Password"
-//                                 helperText="make the stars just as happy as before"
-//                             />
-
-//                             <CustomButton variant="outlined" type="submit">
-//                                 Submit
-//                             </CustomButton>
-//                         </Form>
-//                     </Formik>
-//                 </Grid>
-//             </Grid>
-//         </>
-//     );
-// };
-
-// export default SignUp;
+export default SignUp;
