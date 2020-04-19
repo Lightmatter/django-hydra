0ff0f033cf0a5f8f87b31f3eb857cad5852b5c8b
Ben Beecher <ben@lightmatter.com>
2020-04-19 16:44:48 -0400

    login and logout events synced between tabs, csrf protection

--- {{cookiecutter.repo_name}}/src/util/axios.js
+++ {{cookiecutter.repo_name}}/src/util/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
+  xsrfCookieName: '{{cookiecutter.repo_name}}_csrftoken',
+  xsrfHeaderName: 'X-CSRFTOKEN',
   baseURL: 'http://localhost:8000',
   withCredentials: true,
 });
