49bf96b7de6420176e0a0a80386f6fb7f3969d6a
Ben Beecher <ben@lightmatter.com>
2020-05-08 20:07:28 -0400

    you have to actually call isServer

--- {{cookiecutter.repo_name}}/src/util/axios.js
+++ {{cookiecutter.repo_name}}/src/util/axios.js
@@ -2,14 +2,17 @@ import axios from 'axios';
 import getConfig from 'next/config';
 import isServer from 'util/isServer';
 
-const serverBaseURL = process.env.SERVER_BASE_URL || '//127.0.0.1:8000';
+const serverBaseURL = process.env.SERVER_BASE_URL || 'http://127.0.0.1:8000';
 export const clientBaseURL = process.env.API_BASE_URL || '';
 
-export const baseURL = isServer ? serverBaseURL : clientBaseURL;
-
-export default axios.create({
+const http = axios.create({
   xsrfCookieName: '{{cookiecutter.repo_name}}_csrftoken',
   xsrfHeaderName: 'X-CSRFTOKEN',
-  baseURL: baseURL,
   withCredentials: true,
 });
+
+http.interceptors.request.use(function(config) {
+  config.baseURL = isServer() ? serverBaseURL : clientBaseURL;
+  return config;
+});
+export default http;
