0d8eab1107684cae5cc6ff60648582eac21d03ce
Ben Beecher <ben@lightmatter.com>
2020-05-17 22:35:17 -0400

    upgrading to next 9.4

--- {{cookiecutter.repo_name}}/next.config.js
+++ {{cookiecutter.repo_name}}/next.config.js
@@ -2,7 +2,13 @@ const path = require('path');
 const SentryWebpackPlugin = require('@sentry/webpack-plugin');
 
 require('dotenv').config();
-const { SENTRY_DSN, SENTRY_ORG, SENTRY_PROJECT, SENTRY_AUTH_TOKEN, NODE_ENV } = process.env;
+const {
+  NEXT_PUBLIC_SENTRY_DSN,
+  SENTRY_ORG,
+  SENTRY_PROJECT,
+  SENTRY_AUTH_TOKEN,
+  NODE_ENV,
+} = process.env;
 
 module.exports = {
   webpack(config, { isServer, buildId }) {
@@ -11,24 +17,17 @@ module.exports = {
     if (!isServer) {
       config.resolve.alias['@sentry/node'] = '@sentry/browser';
     }
-    if (SENTRY_DSN && SENTRY_ORG && SENTRY_PROJECT && SENTRY_AUTH_TOKEN) {
+    if (NEXT_PUBLIC_SENTRY_DSN && SENTRY_ORG && SENTRY_PROJECT && SENTRY_AUTH_TOKEN) {
       config.plugins.push(
         new SentryWebpackPlugin({
           include: '.next',
           ignore: ['node_modules'],
           urlPrefix: '~/_next',
-          release: process.env.APP_VERSION_RELEASE,
+          release: process.env.NEXT_PUBLIC_APP_VERSION_RELEASE,
         })
       );
     }
     return config;
   },
   poweredByHeader: false,
-  env: {
-    API_BASE_URL: process.env.API_BASE_URL,
-    SERVER_BASE_URL: process.env.SERVER_BASE_URL,
-    SENTRY_DSN: process.env.SENTRY_DSN,
-    APP_VERSION_RELEASE: process.env.APP_VERSION_RELEASE,
-    ENVIRONMENT: process.env.ENVIRONMENT,
-  },
 };
