529ae0b69672d0548912da3d2a12f5f223a23ecb
Ben Beecher <ben@lightmatter.com>
2020-05-19 16:55:15 -0400

    turning off file writing test example

--- {{cookiecutter.repo_name}}/src/__tests__/cypress/integration/examples/files.spec.js
+++ {{cookiecutter.repo_name}}/src/__tests__/cypress/integration/examples/files.spec.js
@@ -91,24 +91,25 @@ context('Files', () => {
 
     // Use a response from a request to automatically
     // generate a fixture file for use later
-    cy.request('https://jsonplaceholder.cypress.io/users')
-      .then((response) => {
-        cy.writeFile('cypress/fixtures/users.json', response.body)
-      })
-    cy.fixture('users').should((users) => {
-      expect(users[0].name).to.exist
-    })
+    // cy.request('https://jsonplaceholder.cypress.io/users')
+    //   .then((response) => {
+    //     cy.writeFile('cypress/fixtures/users.json', response.body)
+    //   })
+    // cy.fixture('users').should((users) => {
+    //   expect(users[0].name).to.exist
+    // })
 
-    // JavaScript arrays and objects are stringified
-    // and formatted into text.
-    cy.writeFile('cypress/fixtures/profile.json', {
-      id: 8739,
-      name: 'Jane',
-      email: 'jane@example.com',
-    })
+    // // JavaScript arrays and objects are stringified
+    // // and formatted into text.
+    // cy.writeFile('cypress/fixtures/profile.json', {
+    //   id: 8739,
+    //   name: 'Jane',
+    //   email: 'jane@example.com',
+    // })
 
-    cy.fixture('profile').should((profile) => {
-      expect(profile.name).to.eq('Jane')
-    })
+
+    // cy.fixture('profile').should((profile) => {
+    //   expect(profile.name).to.eq('Jane')
+    // })
   })
 })
