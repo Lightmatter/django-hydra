da9ebe393f0ec5645d593b48b61efd0005f09c08
Brian Edelman <bd.edelman@gmail.com>
2020-05-19 16:24:57 -0400

    updates to login page and signup page, reusable password field with toggleable text, required minimum nodejs in package,json, adds grid to header, removes unused public folder

---
+++ {{cookiecutter.repo_name}}/src/components/PasswordField.jsx
@@ -0,0 +1,45 @@
+import {useState} from 'react';
+import {
+    IconButton,
+    InputAdornment,
+} from '@material-ui/core';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+
+import { Field } from 'formik';
+import { TextField } from 'formik-material-ui';
+
+import Link from '@material-ui/core/Link';
+
+export default function PasswordField({ ...rest }) {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleClickShowPassword = () => {
+        setShowPassword(prevShowPassword => !prevShowPassword);
+    };
+
+    const handleMouseDownPassword = event => {
+        event.preventDefault();
+    };
+    return (
+        <Field
+            component={TextField}
+            type={showPassword ? 'text' : 'password'}
+            {...rest}
+            data-cy="password"
+            InputProps={{
+                endAdornment: (
+                    <InputAdornment position="end">
+                        <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                        >
+                            {showPassword ? <Visibility /> : <VisibilityOff />}
+                        </IconButton>
+                    </InputAdornment>
+                ),
+            }}
+        />
+    );
+}
--- {{cookiecutter.repo_name}}/src/components/header.jsx
+++ {{cookiecutter.repo_name}}/src/components/header.jsx
@@ -1,16 +1,12 @@
-import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
+import { AppBar, Toolbar, Typography, IconButton, Menu, MenuItem, Grid } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
 import { usePopupState, bindTrigger, bindMenu } from 'material-ui-popup-state/hooks';
+
 import Link from 'components/router/Link';
 import { useIsAuthenticated, useCurrentUser, logOut } from 'models/user';
+
 const useStyles = makeStyles(theme => ({
     menuButton: {
         marginRight: theme.spacing(2),
@@ -45,54 +41,61 @@ export default function MenuAppBar() {
                 >
                     <MenuIcon />
                 </IconButton>
-                <Menu keepMounted {...bindMenu(navMenuState)}>
-                    <MenuItem onClick={navMenuState.close}>
-                        <Link data-cy="login" href="/login">
-                            Login
-                        </Link>
-                    </MenuItem>
-                    <MenuItem onClick={navMenuState.close}>
-                        <Link data-cy="signup" href="/signup">
-                            Sign Up
-                        </Link>
-                    </MenuItem>
-                    <MenuItem onClick={throwException}>Throw an error</MenuItem>
-                </Menu>
-                <Link href="/">
-                    <Typography variant="h6" color="textPrimary" className={classes.title}>
-                        Lightmatter
-                    </Typography>
-                </Link>
-                {user && (
-                    <div>
-                        <span data-cy="logged-in-name">Hey {user.first_name}</span>
-                        <IconButton
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            color="inherit"
-                            {...bindTrigger(profileMenuState)}
-                        >
-                            <AccountCircle />
-                        </IconButton>
-                        <Menu id="menu-appbar" {...bindMenu(profileMenuState)}>
-                            <MenuItem onClick={profileMenuState.close}>
-                                <Link href="/account">Profile</Link>
+                <Grid container justify="space-between" alignItems="center">
+                    <Grid item xs={6} md={3}>
+                        <Menu keepMounted {...bindMenu(navMenuState)}>
+                            <MenuItem onClick={navMenuState.close}>
+                                <Link data-cy="login" href="/login">
+                                    Login
+                                </Link>
                             </MenuItem>
-                            <MenuItem onClick={profileMenuState.close}>
-                                <Link href="/account/change-password">Change Password</Link>
+                            <MenuItem onClick={navMenuState.close}>
+                                <Link data-cy="signup" href="/signup">
+                                    Sign Up
+                                </Link>
                             </MenuItem>
-                            <MenuItem onClick={profileMenuState.close}>
-                                <Link href="/account/change-email">Change Email</Link>
-                            </MenuItem>
-                            <MenuItem onClick={profileMenuState.close}>
-                                <Link href="/account/delete-account">Delete Account</Link>
-                            </MenuItem>
-
-                            <MenuItem onClick={logoutButtonClick}>Logout</MenuItem>
+                            <MenuItem onClick={throwException}>Throw an error</MenuItem>
                         </Menu>
-                    </div>
-                )}
+                        <Link href="/">
+                            <Typography variant="h6" color="textPrimary" className={classes.title}>
+                                Lightmatter
+                            </Typography>
+                        </Link>
+                    </Grid>
+                    <Grid item xs={6} md={9} align="right">
+
+                        {user && (
+                            <div>
+                                <span data-cy="logged-in-name">Hey {user.first_name}</span>
+                                <IconButton
+                                    aria-label="account of current user"
+                                    aria-controls="menu-appbar"
+                                    aria-haspopup="true"
+                                    color="inherit"
+                                    {...bindTrigger(profileMenuState)}
+                                >
+                                    <AccountCircle />
+                                </IconButton>
+                                <Menu id="menu-appbar" {...bindMenu(profileMenuState)}>
+                                    <MenuItem onClick={profileMenuState.close}>
+                                        <Link href="/account">Profile</Link>
+                                    </MenuItem>
+                                    <MenuItem onClick={profileMenuState.close}>
+                                        <Link href="/account/change-password">Change Password</Link>
+                                    </MenuItem>
+                                    <MenuItem onClick={profileMenuState.close}>
+                                        <Link href="/account/change-email">Change Email</Link>
+                                    </MenuItem>
+                                    <MenuItem onClick={profileMenuState.close}>
+                                        <Link href="/account/delete-account">Delete Account</Link>
+                                    </MenuItem>
+
+                                    <MenuItem onClick={logoutButtonClick}>Logout</MenuItem>
+                                </Menu>
+                            </div>
+                        )}
+                    </Grid>
+                </Grid>
             </Toolbar>
         </AppBar>
     );
