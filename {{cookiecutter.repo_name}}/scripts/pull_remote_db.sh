c29535a1e8eda73dcf2654a110b55143766490ed
Brian Edelman <bd.edelman@gmail.com>
2020-09-22 13:43:11 -0400

    fixing bug with db pull script if terraform was used to set up heroku with 'development' instead of 'dev'

--- {{cookiecutter.repo_name}}/scripts/pull_remote_db.sh
+++ {{cookiecutter.repo_name}}/scripts/pull_remote_db.sh
@@ -3,6 +3,6 @@
 source scripts/get-env-secrets.sh;
 
 dropdb $ENV_NAME
-heroku pg:pull DATABASE_URL $ENV_NAME --app $ENV_NAME-dev
+heroku pg:pull DATABASE_URL $ENV_NAME --app $ENV_NAME-development
 for tbl in `psql -qAt -c "select tablename from pg_tables where schemaname = 'public';" $ENV_NAME` ; do  psql -c "alter table \"$tbl\" owner to $ENV_NAME" $ENV_NAME ; done
 for tbl in `psql -qAt -c "select sequence_name from information_schema.sequences where sequence_schema = 'public';" $ENV_NAME` ; do  psql -c "alter table \"$tbl\" owner to $ENV_NAME" $ENV_NAME ; done
