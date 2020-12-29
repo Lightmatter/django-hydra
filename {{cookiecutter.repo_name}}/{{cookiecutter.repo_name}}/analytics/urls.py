d6ed9861b48d041e06ddb4c35a0fe7f76891b12c
Jason Bryant <jason@lightmatter.com>
2020-12-29 15:54:10 +0000

    resolve indentation for test.sh

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/analytics/urls.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/analytics/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('group', views.group, name='group'),
-    path('view', views.view, name='view'),
-    path('track', views.track, name='track'),
+    path("group", views.group, name="group"),
+    path("view", views.view, name="view"),
+    path("track", views.track, name="track"),
 ]
