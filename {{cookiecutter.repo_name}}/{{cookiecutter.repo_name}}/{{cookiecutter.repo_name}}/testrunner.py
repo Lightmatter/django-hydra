46525c077d5e7d4294e70f2e2e1d971302d143d1
Ben Beecher <Ben@lightmatter.com>
2021-03-04 18:12:13 -0500

    making pylint happy

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/testrunner.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/testrunner.py
@@ -11,4 +11,4 @@ class TestRunner(DiscoverRunner):
             if "integration" not in exclude_tags:
                 exclude_tags.append("integration")
         kwargs["exclude_tags"] = exclude_tags
-        return super().__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
