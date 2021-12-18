2001fde4f273bd93a7d830c49c6ecc8cdaaf6f19
Ben Beecher <Ben@lightmatter.com>
2021-12-18 13:22:57 -0500

    adding formfield component

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/util/forms.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/util/forms.py
@@ -1,1 +1,9 @@
+from django_components import component
 
+
+@component.register("form_field")
+class FormField(component.Component):
+    template_name = "components/form_field.html"
+
+    def get_context_data(self, **kwargs):
+        return {**kwargs}
