90c2ecafc443298eb1df22772b2660e5a1854fca
Ben Beecher <Ben@lightmatter.com>
2022-01-04 17:36:33 -0500

    adding prettier

--- {{cookiecutter.repo_name}}/frontend/js/main.ts
+++ {{cookiecutter.repo_name}}/frontend/js/main.ts
@@ -1,13 +1,12 @@
 /* eslint-disable import/first */
 
 // @ts-expect-error
-if (import.meta.env.MODE !== 'development') {
+if (import.meta.env.MODE !== "development") {
     // @ts-expect-error
-    import('vite/modulepreload-polyfill')
+    import("vite/modulepreload-polyfill");
 }
 
-
 // Import our CSS
-import '/css/app-base.css';
-import '/css/app-components.css';
-import '/css/app-utilities.css';
+import "/css/app-base.css";
+import "/css/app-components.css";
+import "/css/app-utilities.css";
--- {{cookiecutter.repo_name}}/frontend/js/system.ts
+++ {{cookiecutter.repo_name}}/frontend/js/system.ts
@@ -1,19 +1,19 @@
-import {createApp, reactive} from 'petite-vue';
-import 'htmx.org';
-const store = reactive({})
+import { createApp, reactive } from "petite-vue";
+import "htmx.org";
+const store = reactive({});
 
 const app = createApp({
-  store,
-  $delimiters: ['${', '}']
+    store,
+    $delimiters: ["${", "}"],
 });
 
 //catch template jitter
 app.mount(document.body);
 
-document.body.addEventListener('htmx:afterSwap', (evt: any) => {
-  app.mount(evt.target);
+document.body.addEventListener("htmx:afterSwap", (evt: any) => {
+    app.mount(evt.target);
 });
 
-document.body.addEventListener('htmx:load', (evt: any) => {
-  app.mount(evt.detail.elt);
+document.body.addEventListener("htmx:load", (evt: any) => {
+    app.mount(evt.detail.elt);
 });
