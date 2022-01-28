0b86f28594511d3e303df0ee50660049288fc108
Andrew Bibby (LightMatter) <97491740+bibby-LM@users.noreply.github.com>
2022-01-28 11:30:03 -0500

    typos (#152)
    * teenie tiny typos
    
    * case-fixed a tiny typo

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/home/views.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/home/views.py
@@ -6,11 +6,11 @@ from django.contrib import messages
 
 
 def error(request):
-    """Generate an exception. Useful for e.g. configuing Sentry"""
+    """Generate an exception. Useful for e.g. configuring Sentry"""
     raise Exception("Make response code 500!")
 
 
 def current_time(request):
-    """Generate the current time. Useful for testing htxm"""
+    """Generate the current time. Useful for testing htmx"""
     messages.info(request, "updated the current time")
     return TemplateResponse(request, "current_time.html")
