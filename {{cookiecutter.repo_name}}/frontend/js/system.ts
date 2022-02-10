4c47d3f53f15d0af82ede783e5bc878c0cbf0c0a
Ben Beecher <Ben@lightmatter.com>
2022-02-09 22:08:50 -0500

    fixing build error

--- {{cookiecutter.repo_name}}/frontend/js/system.ts
+++ {{cookiecutter.repo_name}}/frontend/js/system.ts
@@ -1,39 +1,14 @@
 import "htmx.org";
+// @ts-expect-error // todo: make a types file for x-widget
 import xWidget from "x-widget";
 import Alpine from "alpinejs";
+
+declare global {
+  interface Window {
+    Alpine?: any;
+  }
+}
 
 window.Alpine = Alpine;
 Alpine.plugin(xWidget);
 Alpine.start();
-
-// Alpine.directive('slot',
-//   (el, { expression }, { Alpine }) => {
-//     Alpine.$data(el)[expression] = el.outerHTML;
-//   });
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   // catch template jitter
-//   app.mount(document.body);
-
-//   /* eslint-disable @typescript-eslint/no-explicit-any */
-//   document.body.addEventListener("htmx:afterSwap", (evt: any) => {
-//     app.mount(evt.target);
-//   });
-
-//   /* eslint-disable @typescript-eslint/no-explicit-any */
-//   document.body.addEventListener("htmx:load", (evt: any) => {
-//     app.mount(evt.detail.elt);
-//   });
-// });
-
-// class Modal extends HTMLElement {
-//   constructor() {
-//     super();
-//     const template = document.getElementById('modal');
-//     const templateContent = template.content;
-//     const innerHTML = templateContent.cloneNode(true);
-//     this.innerHTML = ''
-//     this.append(innerHTML);
-//   }
-// }
-// customElements.define("alpine-modal",Modal);
