0aa1b25a34810b6930c701d10f60be3d4eec1fa9
Ben Beecher <Ben@lightmatter.com>
2022-03-10 21:00:27 -0500

    adding tailwind plugin for htmx states

--- {{cookiecutter.repo_name}}/tailwind.config.js
+++ {{cookiecutter.repo_name}}/tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   content: [
     './frontend/**/*.html',
@@ -8,5 +10,12 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addVariant }) {
+      addVariant('htmx-settling', ['&.htmx-settling', '.htmx-settling &']);
+      addVariant('htmx-request',  ['&.htmx-request',  '.htmx-request &']);
+      addVariant('htmx-swapping', ['&.htmx-swapping', '.htmx-swapping &']);
+      addVariant('htmx-added',    ['&.htmx-added',    '.htmx-added &']);
+    }),
+  ],
 }
