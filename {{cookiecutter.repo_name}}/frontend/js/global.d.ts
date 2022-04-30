38de74817666f2f85eb6ac4a788f7e7062b0bfe6
Ben Beecher <Ben@lightmatter.com>
2022-04-30 13:02:43 -0400

    updating to use global types

---
+++ {{cookiecutter.repo_name}}/frontend/js/global.d.ts
@@ -0,0 +1,5 @@
+import { Alpine as AlpineType } from "alpinejs"
+
+declare global {
+  const Alpine: AlpineType;
+}
--- {{cookiecutter.repo_name}}/frontend/js/main.ts
+++ {{cookiecutter.repo_name}}/frontend/js/main.ts
@@ -6,11 +6,6 @@ import "@/css/app-utilities.css";
 import "htmx.org";
 import Alpine from "alpinejs";
 
-declare global {
-  interface Window {
-    Alpine?: any;
-  }
-}
 
 // @ts-expect-error // needs to declare that htmx lives on window, auto added by import
 const { htmx } = window; // eslint-disable-line  @typescript-eslint/no-unused-vars
