7161a2b785794d313d1ac883c0b6109ba60318e0
Alex Krupp <alex@lightmatter.com>
2022-10-25 14:56:23 -0400

    Reorganize components.

--- {{cookiecutter.repo_name}}/vite.config.js
+++ {{cookiecutter.repo_name}}/vite.config.js
@@ -4,20 +4,27 @@ import { defineConfig } from 'vite';
 
 
 export default defineConfig({
+  // Where the project's files are
+  root: resolve('./static_source/'),
+  // The public path where assets are served, both in development and in production.
   base: "/static/",
-  root: resolve('./frontend/'),
   resolve:{
     alias:{
-      '@' : resolve('./frontend')
+      // Use '@' in urls as a shortcut for './static_source'. (Currently used in CSS files.)
+      '@' : resolve('./static_source')
     },
   },
   build: {
     manifest: true, // adds a manifest.json
     rollupOptions: {
       input: {
-        main: resolve(__dirname, './frontend/js/main.ts'),
-        base: resolve(__dirname, './frontend/css/base.js'),
-        raw_tailwind: resolve(__dirname, './frontend/css/tailwind.js'),
+        /* The bundle's entry point(s).  If you provide an array of entry points or an object mapping names to 
+        entry points, they will be bundled to separate output chunks. */
+        main: resolve(__dirname, './frontend_comp/templates/components/main.ts'),
+        base: resolve(__dirname, './static_source/css/base.js'),
+        raw_tailwind: resolve(__dirname, './static_source/css/tailwind.js'),
+        // Components go here
+        flyout: resolve(__dirname, './frontend_comp/templates/components/flyout/flyout.ts')
       }
     },
     outDir:  '../{{cookiecutter.repo_name}}/static_source/', // puts the manifest.json in PROJECT_ROOT/static_source/
@@ -26,12 +33,12 @@ export default defineConfig({
     {
       name: 'watch-external', // https://stackoverflow.com/questions/63373804/rollup-watch-include-directory/63548394#63548394
       async buildStart(){
-        const htmls = await fg(['{{cookiecutter.repo_name}}/templates/**/*.html']);
+        const htmls = await fg(['frontend_comp/templates/**/*.html']);
         for(let file of htmls){
           this.addWatchFile(file);
         }
 
-        const jinjas = await fg(['{{cookiecutter.repo_name}}/templates/**/*.jinja']);
+        const jinjas = await fg(['frontend_comp/templates/**/*.jinja']);
         for(let file of jinjas){
           this.addWatchFile(file);
         }
