6e8226b5f5510222fd911bec6616b0289c499d9b
Tom Carrick <tom@carrick.eu>
2022-11-28 15:16:22 +0100

    Add comment

--- {{cookiecutter.repo_name}}/gunicorn.conf.py
+++ {{cookiecutter.repo_name}}/gunicorn.conf.py
@@ -4,5 +4,6 @@ accesslog = "-"
 bind = "0.0.0.0:8080"
 preload_app = True
 worker_class = "uvicorn.workers.UvicornWorker"
+# See https://docs.gunicorn.org/en/stable/design.html#how-many-workers
 workers = multiprocessing.cpu_count() * 2 + 1
 wsgi_app = "{{cookiecutter.repo_name}}.config.asgi:application"
