d196746b54ee6fea2060f2b78f13e348bd25fa0c
Tom Carrick <tom@carrick.eu>
2022-12-05 10:30:14 +0100

    and urls

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/home/urls.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/home/urls.py
@@ -4,14 +4,14 @@ from django.views.generic.base import TemplateView
 from .views import current_time, error, test_message_redirect, test_message_refresh
 
 urlpatterns = [
-    path(r"current-time/", current_time, name="current_time"),
-    path(r"test-refresh/", test_message_refresh, name="test_refresh"),
-    path(r"test-redirect/", test_message_redirect, name="test_redirect"),
+    path("current-time/", current_time, name="current_time"),
+    path("test-refresh/", test_message_refresh, name="test_refresh"),
+    path("test-redirect/", test_message_redirect, name="test_redirect"),
     path(
         "random-chart/",
         TemplateView.as_view(template_name="samples/random_chart.jinja"),
         name="random_chart",
     ),
-    path(r"error/", error, name="error"),
-    path(r"", TemplateView.as_view(template_name="index.jinja"), name="home"),
+    path("error/", error, name="error"),
+    path("", TemplateView.as_view(template_name="index.jinja"), name="home"),
 ]
