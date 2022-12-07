47088307bc85d6dec32c2424c02fe6e043a13278
Tom Carrick <tom@carrick.eu>
2022-12-07 21:54:07 +0100

    Pre-commit updates and related changes (#284)
    * Pre-commit updates and related changes
    
    * and urls

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/user/models.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/user/models.py
@@ -1,7 +1,6 @@
 from django.contrib import auth
 from django.contrib.auth.hashers import make_password
-from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
-                                        PermissionsMixin)
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.core.mail import send_mail
 from django.db import models
 from django.utils.translation import gettext_lazy as _
