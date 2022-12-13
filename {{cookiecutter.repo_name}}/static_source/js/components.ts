5ede46797325378d88aaea5b81f9dff697672b5e
Jacob Kent <kentjakel@wustl.edu>
2022-12-13 16:38:50 -0500

    fix tests

--- {{cookiecutter.repo_name}}/static_source/js/components.ts
+++ {{cookiecutter.repo_name}}/static_source/js/components.ts
@@ -1,8 +1,7 @@
 import glob from "glob";
 import { resolve } from "path";
 
-const path = resolve(__dirname, "./{{cookiecutter.repo_name}}/templates/components/**/*{.js, .ts}");
-glob(path, options, (er, files) => {
+glob(resolve(__dirname, "./{{cookiecutter.repo_name}}/templates/components/**/*{.js, .ts}"), (er, files) => {
   // eslint-disable-next-line no-restricted-syntax
   for (const file of files) {
     import(file);
