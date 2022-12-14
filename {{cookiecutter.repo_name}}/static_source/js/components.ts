7ad1f4be94e949ff836d4a9f5cedb789bfb0e641
Jacob Kent <kentjakel@wustl.edu>
2022-12-14 17:38:49 -0500

    consolidate the static_source folders

--- {{cookiecutter.repo_name}}/static_source/js/components.ts
+++
@@ -1,3 +0,0 @@
-import.meta.glob("@/../jlktestone/templates/components/**/*.js", { eager: true });
-import.meta.glob("@/../jlktestone/templates/components/**/*.ts", { eager: true });
-
---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/static_source/js/components.ts
@@ -0,0 +1,3 @@
+import.meta.glob("@/../templates/components/**/*.js", { eager: true });
+import.meta.glob("@/../templates/components/**/*.ts", { eager: true });
+
