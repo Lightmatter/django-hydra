70a98b1f1117daa6ae72f2f272840fec119fcfb5
Jacob Kent <kentjakel@wustl.edu>
2022-12-15 13:07:33 -0500

    convert modal and flyout to jinja macro/alpine headless components

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/static_source/js/main.ts
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/static_source/js/main.ts
@@ -1,5 +1,6 @@
 import focus from "@alpinejs/focus";
 import mask from "@alpinejs/mask";
+import ui from "@alpinejs/ui";
 
 import "htmx.org";
 import Alpine from "alpinejs";
@@ -22,7 +23,7 @@ htmx.config.scrollBehavior = "auto";
 
 
 htmx.defineExtension("get-csrf", {
-  onEvent: function (name: string, evt: any) {
+  onEvent(name: string, evt: any) {
     if (name === "htmx:configRequest") {
       evt.detail.headers["X-CSRFToken"] = Cookies.get("{{cookiecutter.repo_name}}_csrftoken");
     }
@@ -38,5 +39,6 @@ if (import.meta.hot) {
 
 window.Alpine = Alpine;
 Alpine.plugin(focus);
-Alpine.plugin(mask)
+Alpine.plugin(mask);
+Alpine.plugin(ui);
 Alpine.start();
