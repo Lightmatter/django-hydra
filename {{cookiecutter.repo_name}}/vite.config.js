6a2503b5012d5707d1058e902feefad0bdf96fa7
Will Wernert <will.w@lightmatter.com>
2023-02-08 10:53:04 -0500

    Format vite.config.js

--- {{cookiecutter.repo_name}}/vite.config.js
+++ {{cookiecutter.repo_name}}/vite.config.js
@@ -8,17 +8,17 @@ export default defineConfig({
   root: resolve('./{{cookiecutter.repo_name}}/static_source/'),
   // The public path where assets are served, both in development and in production.
   base: "/static/",
-  resolve:{
-    alias:{
+  resolve: {
+    alias: {
       // Use '@' in urls as a shortcut for './static_source'. (Currently used in CSS files.)
-      '@' : resolve('./{{cookiecutter.repo_name}}/static_source')
+      '@': resolve('./{{cookiecutter.repo_name}}/static_source')
     },
   },
   build: {
     manifest: true, // adds a manifest.json
     rollupOptions: {
       input: {
-        /* The bundle's entry point(s).  If you provide an array of entry points or an object mapping names to 
+        /* The bundle's entry point(s).  If you provide an array of entry points or an object mapping names to
         entry points, they will be bundled to separate output chunks. */
         components: resolve(__dirname, './{{cookiecutter.repo_name}}/static_source/js/components.ts'),
         account: resolve(__dirname, './{{cookiecutter.repo_name}}/static_source/js/account.ts'),
@@ -29,19 +29,19 @@ export default defineConfig({
         raw_tailwind: resolve(__dirname, './{{cookiecutter.repo_name}}/static_source/css/tailwind.js'),
       }
     },
-    outDir:  './', // puts the manifest.json in PROJECT_ROOT/static_source/ for Django to collect
+    outDir: './', // puts the manifest.json in PROJECT_ROOT/static_source/ for Django to collect
   },
   plugins: [
     {
       name: 'watch-external', // https://stackoverflow.com/questions/63373804/rollup-watch-include-directory/63548394#63548394
-      async buildStart(){
+      async buildStart() {
         const htmls = await fg(['{{cookiecutter.repo_name}}/templates/**/*.html']);
-        for(let file of htmls){
+        for (let file of htmls) {
           this.addWatchFile(file);
         }
 
         const jinjas = await fg(['{{cookiecutter.repo_name}}/templates/**/*.jinja']);
-        for(let file of jinjas){
+        for (let file of jinjas) {
           this.addWatchFile(file);
         }
       }
@@ -49,7 +49,7 @@ export default defineConfig({
     {
       name: 'reloadHtml',
       handleHotUpdate({ file, server }) {
-        if (file.endsWith('.html') || file.endsWith('.jinja') ){
+        if (file.endsWith('.html') || file.endsWith('.jinja')) {
           server.ws.send({
             type: 'custom',
             event: 'template-hmr',
