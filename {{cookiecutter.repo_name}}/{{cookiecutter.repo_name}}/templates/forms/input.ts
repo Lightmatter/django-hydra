563daef78703a83c9496b213c1021d6478e293b2
Will Wernert <will.w@lightmatter.com>
2023-02-08 15:12:52 -0500

    Reuse arrow functions, typecast to match expected type

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/templates/forms/input.ts
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/templates/forms/input.ts
@@ -1,23 +1,22 @@
 import AlpineInstance, { AlpineComponent } from "alpinejs";
+import { AlpineDataCallback } from "../../static_source/js";
 
-function input(eventName: string, value: string): AlpineComponent {
-  return {
-    eventName,
-    value,
-    init() {
-      if (this.value === "None") {
-        this.value = "";
-      }
-      if (this.eventName !== "input") {
-        this.$watch("value", () => {
-          this.$dispatch(
-            this.eventName,
-            { value: this.value },
-          );
-        });
-      }
-    },
-  };
-}
+const input = (eventName: string, value: string): AlpineComponent => ({
+  eventName,
+  value,
+  init() {
+    if (this.value === "None") {
+      this.value = "";
+    }
+    if (this.eventName !== "input") {
+      this.$watch("value", () => {
+        this.$dispatch(
+          this.eventName,
+          { value: this.value },
+        );
+      });
+    }
+  },
+});
 
-AlpineInstance.data("input", input);
+AlpineInstance.data("input", input as AlpineDataCallback);
