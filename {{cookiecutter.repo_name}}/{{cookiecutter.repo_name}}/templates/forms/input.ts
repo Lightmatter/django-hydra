a96e7d2ebd85df0fc3157460c5d803d0005d66db
Will Wernert <will.w@lightmatter.com>
2023-02-08 15:08:15 -0500

    Don't use arrow functions for alpine components
    If we don't want to use a spread of an array of unknown type, we need to use actual functions to satisfy the type requirements for Alpine

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/templates/forms/input.ts
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/templates/forms/input.ts
@@ -1,22 +1,23 @@
 import AlpineInstance, { AlpineComponent } from "alpinejs";
 
-const input = (...args: unknown[]): AlpineComponent => ({
-  // Props
-  eventName: args[0] as string,
-  value: args[1] as string,
-  init() {
-    if (this.value === "None") {
-      this.value = "";
-    }
-    if (this.eventName !== "input") {
-      this.$watch("value", () => {
-        this.$dispatch(
-          this.eventName,
-          { value: this.value },
-        );
-      });
-    }
-  },
-});
+function input(eventName: string, value: string): AlpineComponent {
+  return {
+    eventName,
+    value,
+    init() {
+      if (this.value === "None") {
+        this.value = "";
+      }
+      if (this.eventName !== "input") {
+        this.$watch("value", () => {
+          this.$dispatch(
+            this.eventName,
+            { value: this.value },
+          );
+        });
+      }
+    },
+  };
+}
 
 AlpineInstance.data("input", input);
--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/templates/forms/password.ts
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/templates/forms/password.ts
@@ -1,23 +1,24 @@
 import AlpineInstance, { AlpineComponent } from "alpinejs";
 
-const password = (...args: unknown[]): AlpineComponent => ({
-  // Props
-  eventName: args[0] as string,
-  value: args[1] as string,
-  type: args[2] as string,
-  init() {
-    if (this.value === "None") {
-      this.value = "";
-    }
-    if (this.eventName !== "") {
-      this.$watch("value", () => {
-        this.$dispatch(
-          this.eventName,
-          { value: this.value },
-        );
-      });
-    }
-  },
-});
+function password(eventName: string, value: string, type: string): AlpineComponent {
+  return {
+    eventName,
+    value,
+    type,
+    init() {
+      if (this.value === "None") {
+        this.value = "";
+      }
+      if (this.eventName !== "") {
+        this.$watch("value", () => {
+          this.$dispatch(
+            this.eventName,
+            { value: this.value },
+          );
+        });
+      }
+    },
+  };
+}
 
 AlpineInstance.data("password", password);
