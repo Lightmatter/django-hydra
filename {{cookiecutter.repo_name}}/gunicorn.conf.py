f0111a42509fe80f692c547619576bfc9298de97
Brian Edelman <brianedelman@brians-mbp.myfiosgateway.com>
2023-04-20 16:32:40 -0400

    adding back dynamic variable

--- {{cookiecutter.repo_name}}/gunicorn.conf.py
+++ {{cookiecutter.repo_name}}/gunicorn.conf.py
@@ -8,3 +8,4 @@ worker_tmp_dir = "/dev/shm"  # noqa
 # See https://docs.gunicorn.org/en/stable/design.html#how-many-workers
 workers = multiprocessing.cpu_count() * 2 + 1
 wsgi_app = "cofertility.config.wsgi:application"
+wsgi_app = "{{cookiecutter.repo_name}}.config.wsgi:application"
