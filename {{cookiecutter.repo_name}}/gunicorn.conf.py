cb3aebc7dcb460583a5bf6f7df88137f1f36525c
Brian Edelman <brianedelman@brians-mbp.myfiosgateway.com>
2023-04-20 16:28:17 -0400

    swtiching to gthread wsgi app, and defaulting render.yaml startCommand to use gunicorn.conf.py

--- {{cookiecutter.repo_name}}/gunicorn.conf.py
+++ {{cookiecutter.repo_name}}/gunicorn.conf.py
@@ -3,7 +3,8 @@ import multiprocessing
 accesslog = "-"
 bind = "0.0.0.0:8080"
 preload_app = True
-worker_class = "uvicorn.workers.UvicornWorker"
+worker_class = "gthread"
+worker_tmp_dir = "/dev/shm"  # noqa
 # See https://docs.gunicorn.org/en/stable/design.html#how-many-workers
 workers = multiprocessing.cpu_count() * 2 + 1
-wsgi_app = "{{cookiecutter.repo_name}}.config.asgi:application"
+wsgi_app = "cofertility.config.wsgi:application"
