8e81e6bb5c33e9c265ffebdb09a5a7180281e7c5
William Wernert <will.w@lightmatter.com>
2023-04-24 15:16:29 -0400

    Add django-robots (#355)
    * Add django-robots
    
    * Revert unrelated fix
    
    * Remove robots config

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/config/settings/base.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/config/settings/base.py
@@ -79,6 +79,7 @@ THIRD_PARTY_APPS = [
     "heroicons",
     "hijack",
     "hijack.contrib.admin",
+    "robots"
 ]
 LOCAL_APPS = [
     "{{cookiecutter.repo_name}}.home.apps.HomeConfig",
@@ -341,4 +342,4 @@ except ImportError:  # Graceful fallback if IceCream isn't installed.
     except ImportError:
         builtins = __import__("builtins")
     ic = lambda *a: None if not a else (a[0] if len(a) == 1 else a)  # noqa
-    setattr(builtins, "ic", ic)  # noqa
+    setattr(builtins, "ic", ic)
