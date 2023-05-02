7dff09394a4d3bbff9dff5330770e6ea0e9e1674
Melissa Felch <melissa@lightmatter.com>
2023-05-02 11:08:55 -0400

    backporting class logic

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/templates/forms/password.ts
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/templates/forms/password.ts
@@ -6,16 +6,57 @@ const password = (eventName: unknown, value: unknown, type: unknown): AlpineComp
   value,
   type,
   active: false,
-  updateFlag() {
+  updateFlag(e:Event) {
     const childInput:HTMLInputElement|null = this.$refs.input.querySelector("input, textarea");
+    // escape hatch for if the function's been attached to something that isn't an input
+    // (this should never happen!)
+
     if (!childInput) {
       return;
     }
-    if (!childInput.value) {
-      this.active = !this.active;
+
+    // if the input is focused, or has a value, set the active flag to true
+    // otherwise, set it to false
+    switch (e.type) {
+      case "focus":
+      case "input":
+        this.active = true;
+        break;
+      case "blur":
+        if (!childInput.value) {
+          this.active = false;
+        } else {
+          this.active = true;
+        }
+        break;
+      default:
     }
   },
   init() {
+    const childInput:HTMLInputElement|null = this.$refs.input.querySelector("input, textarea");
+    if (childInput) {
+      this.$refs.input.addEventListener("animationstart", (e) => {
+        switch (e.animationName) {
+          case "autofill-start":
+            this.active = true;
+            this.$refs.input.classList.add("autofilled");
+            break;
+          case "autofill-cancel":
+            if (childInput === document.activeElement) {
+              this.active = true;
+            } else {
+              this.active = false;
+            }
+            if (!childInput.value) {
+              childInput.classList.remove("autofilled");
+            }
+            break;
+          default:
+        }
+      });
+    }
+
+    // We're using the animationstart event to detect when the browser has autofilled
     if (this.value === "None") {
       this.value = "";
     }
