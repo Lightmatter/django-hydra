7809bb8a0cd6d46803cc240738d93c06c56f70de
Brian Edelman <brianedelman@brians-mbp.myfiosgateway.com>
2023-06-23 15:08:33 -0400

    adding split datetimefix

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/templates/forms/date_datetime.ts
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/templates/forms/date_datetime.ts
@@ -3,21 +3,29 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/themes/light.css";
 import { AlpineDataCallback } from "../../static_source/js";
 
-const dateTime = (eventName: string, value: string, enableTime: boolean): AlpineComponent => ({
+const dateTime = (
+  eventName: string,
+  value: string,
+  enableTime: boolean,
+  noCalendar: boolean
+): AlpineComponent => ({
   eventName,
   value,
   enableTime,
+  noCalendar,
   init() {
     if (this.value === "None") {
       this.value = null;
     }
 
     // see https://flatpickr.js.org/formatting/
-    const dateFormat = enableTime ? "m/d/Y H:i" : "m/d/Y";
+    let dateFormat = enableTime ? "m/d/Y H:i" : "m/d/Y";
+    dateFormat = noCalendar ? "H:i" : dateFormat;
 
     const picker = flatpickr(this.$refs.picker, {
       mode: "single",
       enableTime,
+      noCalendar,
       dateFormat,
       defaultDate: value,
       onChange: (_, dateString) => {
