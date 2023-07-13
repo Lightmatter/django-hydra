d540c87faf659ceeaaf3e6fb3d525f37c8cc34c9
alligatortower <mrweed@gmail.com>
2023-07-13 17:22:59 -0400

    fix ruff linting errors

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/util/admin.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/util/admin.py
@@ -5,8 +5,9 @@ from django.forms.renderers import DjangoTemplates
 from django.template.base import mark_safe
 
 
-def force_django_template(AdminClass: type[admin.ModelAdmin]):
-    """ Replaces your admin class with one which forces the use of the DjangoTemplates renderer
+def force_django_template(admin_class: type[admin.ModelAdmin]):
+    """Replaces your admin class with one which forces the useof
+    of the DjangoTemplates renderer
         Noted exceptions:
             - sub-widgets rendered by the RelatedFieldWidgetWrapper
         Usage:
@@ -16,34 +17,34 @@ def force_django_template(AdminClass: type[admin.ModelAdmin]):
                 ...
     """
     action_form_class = None
-    if ActionFormClass := getattr(AdminClass, "action_form", None):
+    if action_form_class := getattr(admin_class, "action_form", None):
 
-        class ForcedDjangoActionForm(ActionFormClass):
+        class ForcedDjangoActionForm(action_form_class):
             default_renderer = DjangoTemplates()
 
         action_form_class = ForcedDjangoActionForm
 
     def get_form(self, request, obj=None, change=False, **kwargs):
-        form = AdminClass.get_form(self, request, obj=obj, change=change, **kwargs)
+        form = admin_class.get_form(self, request, obj=obj, change=change, **kwargs)
         form.default_renderer = DjangoTemplates()
         return form
 
     def changelist_view(self, request, extra_context=None):
-        template_response = AdminClass.changelist_view(
+        template_response = admin_class.changelist_view(
             self, request, extra_context=extra_context
         )
         template_response.using = "django"
         return template_response
 
     def get_changelist_form(self, request, **kwargs):
-        Form = AdminClass.get_changelist_form(self, request, **kwargs)
-        Form.renderer = DjangoTemplates()
-        return Form
+        form = admin_class.get_changelist_form(self, request, **kwargs)
+        form.renderer = DjangoTemplates()
+        return form
 
     def get_changelist_formset(self, request, **kwargs):
-        FormSet = AdminClass.get_changelist_formset(self, request, **kwargs)
-        FormSet.renderer = DjangoTemplates()
-        return FormSet
+        formset = admin_class.get_changelist_formset(self, request, **kwargs)
+        formset.renderer = DjangoTemplates()
+        return formset
 
     @display(description=mark_safe('<input type="checkbox" id="action-toggle">'))
     def action_checkbox(self, obj):
@@ -56,8 +57,8 @@ def force_django_template(AdminClass: type[admin.ModelAdmin]):
         )
 
     return type(
-        f"{AdminClass.__name__}_forceddjangotemplates",
-        (AdminClass,),
+        f"{admin_class.__name__}_forceddjangotemplates",
+        (admin_class,),
         {
             "get_form": get_form,
             "action_form": action_form_class,
