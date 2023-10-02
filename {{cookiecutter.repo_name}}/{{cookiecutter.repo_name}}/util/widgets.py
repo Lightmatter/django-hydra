adaef54db900b2f36a76f22ab1687e71ac7d9807
Brian Edelman <brianedelman@brians-mbp.myfiosgateway.com>
2023-10-02 16:09:40 -0400

    fixing toggle styling and adding docs

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/util/widgets.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/util/widgets.py
@@ -2,5 +2,16 @@ from django.forms.widgets import CheckboxInput
 
 
 class ToggleWidget(CheckboxInput):
+    """
+    Change a CheckboxInput to a Toggle widget
+
+    This will use the html file toggle.html which calls a jinja macro toggle()
+
+    It is modeled after: https://alpinejs.dev/component/toggle
+
+    Usage:
+    agree_terms = forms.BooleanField(widget=ToggleWidget, required=True, label="Agree to terms")
+    """
+
     input_type = "toggle"
     template_name = "django/forms/widgets/toggle.html"
